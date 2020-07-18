5da2361b5228f7769eeb8b241a6337f3fe013897
danilrespati <taufiq.dhanil@gmail.com>
2020-07-18 10:03:10 +0700

    add connection with Blynk

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,36 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 #include "DHTesp.h"
 #include <Servo.h>
 
 #define DHTTYPE DHT22
 #define DHT_PIN D2    //V0=h, V1=t
 #define PIR_PIN D3    //V2=pirState, V3=pirAlert
-#define RELAY1_PIN D4 //V4=relayState
-#define RELAY2_PIN D5 //V5=relayState
+#define RELAY1_PIN D4 //V4=relayLamp
+#define RELAY2_PIN D5 //V5=relayLock
 #define RELAY_ON LOW
 #define SERVO_PIN D6  //V6=servoPos
+
+BLYNK_WRITE(V4)
+{
+  int state = param.asInt();
+  digitalWrite(RELAY1_PIN, !state);
+}
+
+BLYNK_WRITE(V5)
+{
+  int state = param.asInt();
+  digitalWrite(RELAY2_PIN, !state);
+}
 
 DHTesp dht;
 Servo servo;
 
+char auth[] = "udGkXpgDnwhJ6a81s0JKs_YfEkXPwHh7";
+char ssid[] = "PW";
+char pass[] = "Password";
 unsigned long previousMillis = 0;
 long interval = 2000; //min. 2000ms
 float h;
@@ -22,6 +41,7 @@ int servoPos = 0;
 
 void setup() {
   Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
   dht.setup(DHT_PIN, DHTesp::DHTTYPE);
   pinMode(PIR_PIN, INPUT);
   digitalWrite(RELAY1_PIN, !RELAY_ON);
@@ -32,6 +52,7 @@ void setup() {
 }
 
 void loop() {
+  Blynk.run();
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis > interval) {
      previousMillis = currentMillis; 
@@ -47,6 +68,8 @@ void getDhtValue() {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  Blynk.virtualWrite(V0,h);
+  Blynk.virtualWrite(V1,t);
   Serial.print("{\"humidity\": ");
   Serial.print(h);
   Serial.print(", \"temp\": ");
@@ -74,4 +97,5 @@ void switchRelay(char relayPin) {
   int state = digitalRead(relayPin);
   digitalWrite(relayPin, !state);
   Serial.println(state);
+  Blynk.virtualWrite(V4,state);
 }
